034b5b0b464c4796535c2006ba0c29f0f81f6ff7
evan hauk <evanhauk@evans-macbook-pro.local>
2019-06-11 13:32:05 -0700

    Data Persisted using User Defaults Method - Saves data into the apps storage location on the phone

--- ToDo/ToDoListViewController.swift
+++ ToDo/ToDoListViewController.swift
@@ -12,8 +12,16 @@ class ToDoListViewController: UITableViewController {
 
     var itemArray = ["Do this", "Do That", "Nah"]
     
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+            itemArray = items
+        }
+        
     }
 
 
@@ -60,6 +68,9 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
 
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            
             self.tableView.reloadData()
         }
 
